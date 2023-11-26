537636990e16d79c299023147618a24c9c91eab3
AndreTeixeira-21126 <a21126@alunos.ipca.pt>
2023-11-26 15:16:56 +0000

    stand microservice integrated with auth. TODO: Change adapter when auth service is completed

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const PostgreStandRepository = require('./src/repositories/PostgreStandRepositor
 const InMemoryStandRepository = require('./src/repositories/InMemoryStandRepository')
 
 const RabbitMQAdapter = require('./src/adapters/RabbitMQAdapter')
+const AxiosAuthServiceAdapter = require('./src/adapters/AxiosAuthServiceAdapter')
 
 dotenv.config()
 
-const app = makeApp(new PostgreStandRepository(process.env.DATABASE_URL), new RabbitMQAdapter(process.env.RABBIT_MQ_URI || 'amqp://localhost:5672')
-)
+const app = makeApp(new PostgreStandRepository(process.env.DATABASE_URL), new RabbitMQAdapter(process.env.RABBIT_MQ_URI || 'amqp://localhost:5672'))
 
 
 app.listen(process.env.SERVER_PORT || 3000, () => {
