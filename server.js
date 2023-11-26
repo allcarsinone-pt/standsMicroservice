dbc9dc7d6bd65e0c7045be927285a9b24ca51ebf
AndreTeixeira-21126 <a21126@alunos.ipca.pt>
2023-11-26 13:11:30 +0000

    feature: added integration with logs microservice

--- server.js
+++ server.js
@@ -3,12 +3,11 @@ const dotenv = require('dotenv')
 const PostgreStandRepository = require('./src/repositories/PostgreStandRepository')
 const InMemoryStandRepository = require('./src/repositories/InMemoryStandRepository')
 
-//const RabbitMQAdapter = require('./src/adapters/RabbitMQAdapter')
+const RabbitMQAdapter = require('./src/adapters/RabbitMQAdapter')
 
 dotenv.config()
 
-const app = makeApp(new PostgreStandRepository(process.env.DATABASE_URL)
-//, new RabbitMQAdapter(process.env.RABBIT_MQ_URI || 'amqp://localhost:5672')
+const app = makeApp(new PostgreStandRepository(process.env.DATABASE_URL), new RabbitMQAdapter(process.env.RABBIT_MQ_URI || 'amqp://localhost:5672')
 )
 
 
--- src/appBuilder.js
+++ src/appBuilder.js
@@ -3,16 +3,16 @@ const router = require('./routes/StandRouter')
 const RegisterStandController = require('./controllers/RegisterStandController')
 const EditStandController = require('./controllers/EditStandController')
 const DeleteStandController = require('./controllers/DeleteStandController')
-//const LogMockAdapter = require('./adapters/LogMockAdapter')
+const LogMockAdapter = require('./adapters/LogMockAdapter')
 
-function makeApp(standRepository
-    //, logAdapter = new LogMockAdapter()
-    ) {
+function makeApp(standRepository,
+    logAdapter = new LogMockAdapter()) {
     const app = express();
     app.use(express.json());
-    app.set('RegisterStandController', new RegisterStandController(standRepository));
+    app.set('RegisterStandController', new RegisterStandController(standRepository, logAdapter));
     app.set('EditStandController', new EditStandController(standRepository));
     app.set('DeleteStandController', new DeleteStandController(standRepository));
+    app.set('LogAdapter', logAdapter) // Log adapter: ex: rabbitmq
     app.use('/stands', router);
     return app;
 }
